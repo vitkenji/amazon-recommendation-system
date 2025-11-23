4fe98946608bb8ef2ea97e2ee8f80a2d37c771dd
vitkenji <vkz.yamada@gmail.com>
2025-11-22 23:46:10 -0300

    pagerank and degree centralities

--- features-creation/features-creation.py
+++ features-creation/features-creation.py
@@ -1,22 +1,20 @@
 import pandas as pd
 import networkx as nx
+import networkit as nk
+import json
 
-df = pd.read_csv('../dataset/preprocessed_data.csv')
+df = pd.read_csv('../dataset/subsample.csv')
 G = nx.read_gml('../network/reviews_network.gml')
 
-degreeCentrality = nx.degree_centrality(G)
-betCentrality = nx.betweenness_centrality(G)
-cloCentrality = nx.closeness_centrality(G)
-eigCentrality = nx.eigenvector_centrality(G)
-pageRank = nx.pagerank(G)
+nkG = nk.nxadapter.nx2nk(G)
+
+deg = nk.centrality.DegreeCentrality(nkG, normalized=True)
+deg.run()
+degreeCentrality = dict(zip(G.nodes(), deg.scores()))
 
 df['user_deg'] = df['user_id'].map(degreeCentrality)
 df['product_deg'] = df['parent_asin'].map(degreeCentrality)
-df['user_bet'] = df['user_id'].map(betCentrality)
-df['product_bet'] = df['parent_asin'].map(betCentrality)
-df['user_clo'] = df['user_id'].map(cloCentrality)
-df['product_clo'] = df['parent_asin'].map(cloCentrality)
-df['user_eig'] = df['user_id'].map(eigCentrality)
-df['product_eig'] = df['parent_asin'].map(eigCentrality)
-df['user_pagerank'] = df['user_id'].map(pageRankCentrality)
-df['product_pagerank'] = df['parent_asin'].map(pageRankCentrality)
+
+pageRank = nx.pagerank(G)
+df['user_pagerank'] = df['user_id'].map(pageRank)
+df['product_pagerank'] = df['parent_asin'].map(pageRank)
