2899283b96cdae56376069866c4554fbda62dd70
vitkenji <vkz.yamada@gmail.com>
2025-11-29 19:06:07 -0300

    new centralities

--- features-creation/features-creation.py
+++ features-creation/features-creation.py
@@ -12,4 +12,12 @@ pageRank = nx.pagerank(G)
 df['user_pagerank'] = df['user_id'].map(pageRank)
 df['product_pagerank'] = df['parent_asin'].map(pageRank)
 
+cloCentrality = nx.closeness_centrality(G)
+df['user_closeness'] = df['user_id'].map(cloCentrality)
+df['product_closeness'] = df['parent_asin'].map(cloCentrality)
+
+eigCentrality = nx.eigenvector_centrality(G, max_iter=5000, tol=1e-06)
+df['user_eig'] = df['user_id'].map(eigCentrality)
+df['product_eig'] = df['parent_asin'].map(eigCentrality)
+
 df.to_csv('../dataset/new_features.csv', index=False)
